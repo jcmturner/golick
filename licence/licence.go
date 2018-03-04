51c95d8c1b69d6fd22ff80d45a57454d935bfe22
Jonathan Turner <jt@jtnet.co.uk>
2018-03-04 14:29:08 +0000

    licencing

--- licence/licence.go
+++ licence/licence.go
@@ -9,6 +9,7 @@ import (
 	"encoding/base64"
 	"encoding/gob"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/hashicorp/go-uuid"
@@ -18,6 +19,7 @@ type Licence struct {
 	UUID       string
 	ValidFrom  time.Time
 	ValidUntil time.Time
+	RunPeriod  time.Duration
 	Count      int64 // Zero = unlimited
 	Signature  []byte
 }
@@ -37,6 +39,20 @@ func New(key *rsa.PrivateKey, from, until time.Time, count int64) (*Licence, err
 	return &l, nil
 }
 
+func NewRunPeriod(key *rsa.PrivateKey, duration time.Duration, count int64) (*Licence, error) {
+	l := Licence{
+		RunPeriod: duration,
+		Count:     count,
+	}
+	u, err := uuid.GenerateUUID()
+	if err != nil {
+		return &l, err
+	}
+	l.UUID = u
+	l.Sign(key)
+	return &l, nil
+}
+
 func Load(s string) (*Licence, error) {
 	lb, err := base64.StdEncoding.DecodeString(s)
 	if err != nil {
@@ -45,7 +61,7 @@ func Load(s string) (*Licence, error) {
 	var l Licence
 	buf := bytes.NewBuffer(lb)
 	dec := gob.NewDecoder(buf)
-	err = dec.Decode(l)
+	err = dec.Decode(&l)
 	if err != nil {
 		return nil, err
 	}
@@ -62,6 +78,22 @@ func (l *Licence) String() (string, error) {
 	return base64.StdEncoding.EncodeToString(buf.Bytes()), nil
 }
 
+func (l *Licence) Print() string {
+	if l.RunPeriod != 0 {
+		return fmt.Sprintf(`Licence Information:
+UUID: %s
+Runtime Duration: %v
+Valid From: %v
+Valid Until: %v
+Limit: %d`, l.UUID, l.RunPeriod, l.ValidFrom, l.ValidUntil, l.Count)
+	}
+	return fmt.Sprintf(`Licence Information:
+UUID: %s
+Valid From: %v
+Valid Until: %v
+Limit: %d`, l.UUID, l.ValidFrom, l.ValidUntil, l.Count)
+}
+
 func (l *Licence) Valid(pubkey *rsa.PublicKey) (bool, error) {
 	h, err := l.hash()
 	if err != nil {
@@ -71,8 +103,13 @@ func (l *Licence) Valid(pubkey *rsa.PublicKey) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	if time.Now().UTC().After(l.ValidUntil) || time.Now().UTC().Before(l.ValidFrom) {
-		return false, errors.New("outside of licence valid times")
+	if l.RunPeriod == 0 {
+		if time.Now().UTC().After(l.ValidUntil) || time.Now().UTC().Before(l.ValidFrom) {
+			return false, errors.New("outside of licence valid times")
+		}
+	} else {
+		l.ValidFrom = time.Now().UTC()
+		l.ValidUntil = time.Now().UTC().Add(l.RunPeriod)
 	}
 	return true, nil
 }
@@ -95,12 +132,13 @@ func (l *Licence) hash() ([32]byte, error) {
 		UUID:       l.UUID,
 		ValidFrom:  l.ValidFrom,
 		ValidUntil: l.ValidUntil,
+		RunPeriod:  l.RunPeriod,
 		Count:      l.Count,
 		Signature:  []byte{},
 	}
 	buf := new(bytes.Buffer)
 	enc := gob.NewEncoder(buf)
-	err := enc.Encode(nl)
+	err := enc.Encode(&nl)
 	if err != nil {
 		return [32]byte{}, err
 	}
